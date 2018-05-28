6875689017014ee03d8b3042783767fbf74a6e9b
ichsannuur <ichsannuur66@gmail.com>
2018-05-28 15:53:42 +0700

    Edit Flake8

--- kosanapp/kosanapp/backoffice/kelola/forms.py
+++ kosanapp/kosanapp/backoffice/kelola/forms.py
@@ -2,18 +2,18 @@ from django import forms
 from kosanapp.apps.kelola.models import query_kosan
 
 class KelolaForm(forms.ModelForm):
-	class Meta:
-		model = query_kosan
-		fields = ("nama_kosan","alamat_kosan","fasilitas")
+    class Meta:
+        model = query_kosan
+        fields = ("nama_kosan", "alamat_kosan", "fasilitas")
 
 
-	def clean_nama_kosan(self):
-		name = self.cleaned_data['nama_kosan']
+    def clean_nama_kosan(self):
+        name = self.cleaned_data['nama_kosan']
 
-		if self.instance and self.instance.nama_kosan == name:
-			return name
+        if self.instance and self.instance.nama_kosan == name:
+            return name
 
-		if query_kosan.objects.filter(nama_kosan=name).exists():
-			raise forms.ValidationError("kosan dengan nama ini sudah ada")
+        if query_kosan.objects.filter(nama_kosan=name).exists():
+            raise forms.ValidationError("kosan dengan nama ini sudah ada")
 
-		return name
+        return name
--- kosanapp/kosanapp/backoffice/kelola/views.py
+++ kosanapp/kosanapp/backoffice/kelola/views.py
@@ -4,38 +4,36 @@ from kosanapp.backoffice.kelola.forms import KelolaForm
 from django.contrib import messages
 
 def index(request):
-	data = query_kosan.objects.all()
-	return render(request,'backoffice/kelola/index.html', {"datas":data})
+    data = query_kosan.objects.all()
+    return render(request, 'backoffice/kelola/index.html', {"datas": data})
 
 def add(request):
-	form = KelolaForm(data=request.POST or None)
-	context_data = {"form":form,
-					"title":"Tambah Data"}
-	if form.is_valid():
-		form.save()
-		messages.success(request,"Berhasil")
-		return redirect('backoffice:kelola:index')
-		
-	return render(request, 'add.html',context_data)
+    form = KelolaForm(data=request.POST or None)
+    context_data = {"form": form,
+                    "title": "Tambah Data"}
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Berhasil")
+        return redirect('backoffice:kelola:index')
+    return render(request, 'add.html', context_data)
 
 def edit(request,id):
-	kosan = query_kosan.objects.get(id=id)
-	form = KelolaForm(data=request.POST or None, instance=kosan)
+    kosan = query_kosan.objects.get(id=id)
+    form = KelolaForm(data=request.POST or None, instance=kosan)
 
-	if form.is_valid():
-		form.save()
-		messages.success(request, 'Berhasil diubah')
-		return redirect('backoffice:kelola:index')
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Berhasil diubah')
+        return redirect('backoffice:kelola:index')
 
-	context_data = {
-		'form': form
-	}
+    context_data = {
+        'form': form
+    }
 
-	return render(request, 'add.html', context_data)
+    return render(request, 'add.html', context_data)
 
 def delete(request,id):
-	query = query_kosan.objects.get(id=id)
-	query.delete()
-	messages.success(request,"Data Berhasil Dihapus")
-	return redirect('backoffice:kelola:index')
-	
+    query = query_kosan.objects.get(id=id)
+    query.delete()
+    messages.success(request, "Data Berhasil Dihapus")
+    return redirect('backoffice:kelola:index')
