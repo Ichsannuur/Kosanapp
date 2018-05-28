26a579731f86a7266b399f826f4b44e16edb54f1
Ichsannuur <ichsannuur66@gmail.com>
2018-05-28 14:47:02 +0700

    ichsan

--- kosanapp/kosanapp/backoffice/urls.py
+++ kosanapp/kosanapp/backoffice/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^logout/', views.logout, name='logout'),
     url(r'^menu/', include('kosanapp.backoffice.menu.urls', namespace='menu')),
-    url(r'^kelola/', include('kosanapp.backoffice.kelola.urls', namespace='kelola'))
+    url(r'^kelola/', include('kosanapp.backoffice.kelola.urls', namespace='kelola')),
+    url(r'^fasilitas/', include('kosanapp.backoffice.fasilitas.urls', namespace='fasilitas'))
 ]
