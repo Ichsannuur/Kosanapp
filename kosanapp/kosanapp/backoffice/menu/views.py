8bfefe7cacb9c71452aecaa04833ed47765f4f92
Ichsannuur <ichsannuur66@gmail.com>
2018-05-28 11:47:26 +0700

    Thrid Commit

--- kosanapp/kosanapp/backoffice/menu/views.py
+++ kosanapp/kosanapp/backoffice/menu/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from kosanapp.apps.kelola.models import query_kosan
 
 def home(request):
-	return render(request,'base.html')
+	count = query_kosan.objects.count()
+	context_data = {
+		"count" : count
+	}
+	return render(request,'backoffice/menu/index.html', context_data)
